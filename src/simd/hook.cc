b5034d30d3b6f74fab6224ec67b7ef2e9df64018
Yudong Cai <yudong.cai@zilliz.com>
2023-09-11 16:27:13 +0800

    Disable SIMD, always use GENERIC
    Signed-off-by: Yudong Cai <yudong.cai@zilliz.com>

--- src/simd/hook.cc
+++ src/simd/hook.cc
@@ -69,60 +69,61 @@ void
 fvec_hook(std::string& simd_type) {
     static std::mutex hook_mutex;
     std::lock_guard<std::mutex> lock(hook_mutex);
+    simd_type = "GENERIC";
 #if defined(__x86_64__)
-    if (use_avx512 && cpu_support_avx512()) {
-        fvec_inner_product = fvec_inner_product_avx512;
-        fvec_L2sqr = fvec_L2sqr_avx512;
-        fvec_L1 = fvec_L1_avx512;
-        fvec_Linf = fvec_Linf_avx512;
-
-        fvec_norm_L2sqr = fvec_norm_L2sqr_sse;
-        fvec_L2sqr_ny = fvec_L2sqr_ny_sse;
-        fvec_inner_products_ny = fvec_inner_products_ny_sse;
-        fvec_madd = fvec_madd_sse;
-        fvec_madd_and_argmin = fvec_madd_and_argmin_sse;
-
-        simd_type = "AVX512";
-    } else if (use_avx2 && cpu_support_avx2()) {
-        fvec_inner_product = fvec_inner_product_avx;
-        fvec_L2sqr = fvec_L2sqr_avx;
-        fvec_L1 = fvec_L1_avx;
-        fvec_Linf = fvec_Linf_avx;
-
-        fvec_norm_L2sqr = fvec_norm_L2sqr_sse;
-        fvec_L2sqr_ny = fvec_L2sqr_ny_sse;
-        fvec_inner_products_ny = fvec_inner_products_ny_sse;
-        fvec_madd = fvec_madd_sse;
-        fvec_madd_and_argmin = fvec_madd_and_argmin_sse;
-
-        simd_type = "AVX2";
-    } else if (use_sse4_2 && cpu_support_sse4_2()) {
-        fvec_inner_product = fvec_inner_product_sse;
-        fvec_L2sqr = fvec_L2sqr_sse;
-        fvec_L1 = fvec_L1_sse;
-        fvec_Linf = fvec_Linf_sse;
-
-        fvec_norm_L2sqr = fvec_norm_L2sqr_sse;
-        fvec_L2sqr_ny = fvec_L2sqr_ny_sse;
-        fvec_inner_products_ny = fvec_inner_products_ny_sse;
-        fvec_madd = fvec_madd_sse;
-        fvec_madd_and_argmin = fvec_madd_and_argmin_sse;
-
-        simd_type = "SSE4_2";
-    } else {
-        fvec_inner_product = fvec_inner_product_ref;
-        fvec_L2sqr = fvec_L2sqr_ref;
-        fvec_L1 = fvec_L1_ref;
-        fvec_Linf = fvec_Linf_ref;
-
-        fvec_norm_L2sqr = fvec_norm_L2sqr_ref;
-        fvec_L2sqr_ny = fvec_L2sqr_ny_ref;
-        fvec_inner_products_ny = fvec_inner_products_ny_ref;
-        fvec_madd = fvec_madd_ref;
-        fvec_madd_and_argmin = fvec_madd_and_argmin_ref;
-
-        simd_type = "GENERIC";
-    }
+//    if (use_avx512 && cpu_support_avx512()) {
+//        fvec_inner_product = fvec_inner_product_avx512;
+//        fvec_L2sqr = fvec_L2sqr_avx512;
+//        fvec_L1 = fvec_L1_avx512;
+//        fvec_Linf = fvec_Linf_avx512;
+//
+//        fvec_norm_L2sqr = fvec_norm_L2sqr_sse;
+//        fvec_L2sqr_ny = fvec_L2sqr_ny_sse;
+//        fvec_inner_products_ny = fvec_inner_products_ny_sse;
+//        fvec_madd = fvec_madd_sse;
+//        fvec_madd_and_argmin = fvec_madd_and_argmin_sse;
+//
+//        simd_type = "AVX512";
+//    } else if (use_avx2 && cpu_support_avx2()) {
+//        fvec_inner_product = fvec_inner_product_avx;
+//        fvec_L2sqr = fvec_L2sqr_avx;
+//        fvec_L1 = fvec_L1_avx;
+//        fvec_Linf = fvec_Linf_avx;
+//
+//        fvec_norm_L2sqr = fvec_norm_L2sqr_sse;
+//        fvec_L2sqr_ny = fvec_L2sqr_ny_sse;
+//        fvec_inner_products_ny = fvec_inner_products_ny_sse;
+//        fvec_madd = fvec_madd_sse;
+//        fvec_madd_and_argmin = fvec_madd_and_argmin_sse;
+//
+//        simd_type = "AVX2";
+//    } else if (use_sse4_2 && cpu_support_sse4_2()) {
+//        fvec_inner_product = fvec_inner_product_sse;
+//        fvec_L2sqr = fvec_L2sqr_sse;
+//        fvec_L1 = fvec_L1_sse;
+//        fvec_Linf = fvec_Linf_sse;
+//
+//        fvec_norm_L2sqr = fvec_norm_L2sqr_sse;
+//        fvec_L2sqr_ny = fvec_L2sqr_ny_sse;
+//        fvec_inner_products_ny = fvec_inner_products_ny_sse;
+//        fvec_madd = fvec_madd_sse;
+//        fvec_madd_and_argmin = fvec_madd_and_argmin_sse;
+//
+//        simd_type = "SSE4_2";
+//    } else {
+//        fvec_inner_product = fvec_inner_product_ref;
+//        fvec_L2sqr = fvec_L2sqr_ref;
+//        fvec_L1 = fvec_L1_ref;
+//        fvec_Linf = fvec_Linf_ref;
+//
+//        fvec_norm_L2sqr = fvec_norm_L2sqr_ref;
+//        fvec_L2sqr_ny = fvec_L2sqr_ny_ref;
+//        fvec_inner_products_ny = fvec_inner_products_ny_ref;
+//        fvec_madd = fvec_madd_ref;
+//        fvec_madd_and_argmin = fvec_madd_and_argmin_ref;
+//
+//        simd_type = "GENERIC";
+//    }
 #endif
 }
 
