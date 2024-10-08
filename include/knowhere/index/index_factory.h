b6da23f4c16c0b7c2bedcdd6c1a5a014083ef18e
Cai Yudong <yudong.cai@zilliz.com>
2024-10-08 14:25:36 +0800

    Merge index_factory.cc into index_factory.h
    Signed-off-by: Cai Yudong <yudong.cai@zilliz.com>

--- include/knowhere/index/index_factory.h
+++ include/knowhere/index/index_factory.h
@@ -20,26 +20,91 @@
 #include "index_static.h"
 #include "knowhere/index/index.h"
 #include "knowhere/utils.h"
+#include "simd/hook.h"
+#ifdef KNOWHERE_WITH_RAFT
+#include <cuda_runtime_api.h>
+#endif
 
 namespace knowhere {
+#ifdef KNOWHERE_WITH_RAFT
+inline bool
+checkGpuAvailable(const std::string& name) {
+    if (name == "GPU_RAFT_BRUTE_FORCE" || name == "GPU_BRUTE_FORCE" || name == "GPU_RAFT_CAGRA" ||
+        name == "GPU_CAGRA" || name == "GPU_RAFT_IVF_FLAT" || name == "GPU_IVF_FLAT" || name == "GPU_RAFT_IVF_PQ" ||
+        name == "GPU_IVF_PQ") {
+        int count = 0;
+        auto status = cudaGetDeviceCount(&count);
+        if (status != cudaSuccess) {
+            LOG_KNOWHERE_INFO_ << cudaGetErrorString(status);
+            return false;
+        }
+        if (count < 1) {
+            LOG_KNOWHERE_INFO_ << "GPU not available";
+            return false;
+        }
+    }
+    return true;
+}
+#endif
+
 class IndexFactory {
  public:
     template <typename DataType>
     expected<Index<IndexNode>>
-    Create(const std::string& name, const int32_t& version, const Object& object = nullptr);
+    Create(const std::string& name, const int32_t& version, const Object& object = nullptr) {
+        static_assert(KnowhereDataTypeCheck<DataType>::value == true);
+        auto& func_mapping_ = MapInstance();
+        auto key = GetKey<DataType>(name);
+        if (func_mapping_.find(key) == func_mapping_.end()) {
+            LOG_KNOWHERE_ERROR_ << "failed to find index " << key << " in factory";
+            return expected<Index<IndexNode>>::Err(Status::invalid_index_error, "index not supported");
+        }
+        LOG_KNOWHERE_INFO_ << "use key " << key << " to create knowhere index " << name << " with version " << version;
+        auto fun_map_v = (FunMapValue<Index<IndexNode>>*)(func_mapping_[key].get());
+
+#ifdef KNOWHERE_WITH_RAFT
+        if (!checkGpuAvailable(name)) {
+            return expected<Index<IndexNode>>::Err(Status::cuda_runtime_error, "gpu not available");
+        }
+#endif
+        if (name == knowhere::IndexEnum::INDEX_FAISS_SCANN && !faiss::support_pq_fast_scan) {
+            LOG_KNOWHERE_ERROR_ << "SCANN index is not supported on the current CPU model";
+            return expected<Index<IndexNode>>::Err(Status::invalid_index_error,
+                                                   "SCANN index is not supported on the current CPU model");
+        }
+
+        return fun_map_v->fun_value(version, object);
+    }
+
     template <typename DataType>
     const IndexFactory&
-    Register(const std::string& name, std::function<Index<IndexNode>(const int32_t&, const Object&)> func);
+    Register(const std::string& name, std::function<Index<IndexNode>(const int32_t&, const Object&)> func) {
+        static_assert(KnowhereDataTypeCheck<DataType>::value == true);
+        auto& func_mapping_ = MapInstance();
+        auto key = GetKey<DataType>(name);
+        assert(func_mapping_.find(key) == func_mapping_.end());
+        func_mapping_[key] = std::make_unique<FunMapValue<Index<IndexNode>>>(func);
+        return *this;
+    }
+
     static IndexFactory&
-    Instance();
+    Instance() {
+        static IndexFactory factory;
+        return factory;
+    }
+
     typedef std::tuple<std::set<std::pair<std::string, VecType>>, std::set<std::string>> GlobalIndexTable;
     static GlobalIndexTable&
-    StaticIndexTableInstance();
+    StaticIndexTableInstance() {
+        static GlobalIndexTable static_index_table;
+        return static_index_table;
+    }
 
  private:
     struct FunMapValueBase {
         virtual ~FunMapValueBase() = default;
     };
+
     template <typename T1>
     struct FunMapValue : FunMapValueBase {
      public:
@@ -47,21 +112,26 @@ class IndexFactory {
         }
         std::function<T1(const int32_t&, const Object&)> fun_value;
     };
+
+    IndexFactory() = default;
+
     typedef std::map<std::string, std::unique_ptr<FunMapValueBase>> FuncMap;
-    IndexFactory();
     static FuncMap&
-    MapInstance();
+    MapInstance() {
+        static FuncMap func_map;
+        return func_map;
+    }
 };
 
-#define KNOWHERE_FACTOR_CONCAT(x, y) index_factory_ref_##x##y
+#define KNOWHERE_FACTORY_CONCAT(x, y) index_factory_ref_##x##y
 #define KNOWHERE_STATIC_CONCAT(x, y) index_static_ref_##x##y
 
 #define KNOWHERE_REGISTER_STATIC(name, index_node, data_type, ...)               \
     const IndexStaticFaced<data_type>& KNOWHERE_STATIC_CONCAT(name, data_type) = \
         IndexStaticFaced<data_type>::Instance().RegisterStaticFunc<index_node<data_type, ##__VA_ARGS__>>(#name);
 
-#define KNOWHERE_REGISTER_GLOBAL(name, func, data_type)           \
-    const IndexFactory& KNOWHERE_FACTOR_CONCAT(name, data_type) = \
+#define KNOWHERE_REGISTER_GLOBAL(name, func, data_type)            \
+    const IndexFactory& KNOWHERE_FACTORY_CONCAT(name, data_type) = \
         IndexFactory::Instance().Register<data_type>(#name, func)
 
 #define KNOWHERE_SIMPLE_REGISTER_GLOBAL(name, index_node, data_type, ...)                             \
